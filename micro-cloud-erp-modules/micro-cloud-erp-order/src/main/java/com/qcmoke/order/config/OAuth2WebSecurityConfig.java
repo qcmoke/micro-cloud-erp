c8cce2829912446494a74afe1b6820977bdcb12f
qcmoke <1667164190@qq.com>
2020-01-21 16:51:35 +0800

    资源服务器获取token时得到用户信息

--- micro-cloud-erp-modules/micro-cloud-erp-order/src/main/java/com/qcmoke/order/config/OAuth2WebSecurityConfig.java
+++ micro-cloud-erp-modules/micro-cloud-erp-order/src/main/java/com/qcmoke/order/config/OAuth2WebSecurityConfig.java
@@ -1,13 +1,15 @@
 package com.qcmoke.order.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.*;
 
 /**
  * Security配置
@@ -15,6 +17,10 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 @Configuration
 @EnableWebSecurity
 public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Qualifier("userDetailsServiceImpl")
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     @Override
@@ -33,7 +39,20 @@ public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
         remoteTokenServices.setClientId("orderServer");//当前资源服务器对于认证服务器的客户端id
         remoteTokenServices.setClientSecret("123456");//客户端秘钥
         remoteTokenServices.setCheckTokenEndpointUrl("http://localhost:9090/oauth/check_token");//认证服务器token校验地址
+        remoteTokenServices.setAccessTokenConverter(getAccessTokenConverter());
         return remoteTokenServices;
+    }
+
+    /**
+     * 当资源服务器获取token令牌后就会到数据库获取用户信息
+     * @return AccessTokenConverter
+     */
+    private AccessTokenConverter getAccessTokenConverter() {
+        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+        DefaultUserAuthenticationConverter userAuthenticationConverter = new DefaultUserAuthenticationConverter();
+        userAuthenticationConverter.setUserDetailsService(userDetailsService);
+        accessTokenConverter.setUserTokenConverter(userAuthenticationConverter);
+        return accessTokenConverter;
     }
 
 }
--- micro-cloud-erp-modules/micro-cloud-erp-order/src/main/java/com/qcmoke/order/controller/OrderController.java
+++ micro-cloud-erp-modules/micro-cloud-erp-order/src/main/java/com/qcmoke/order/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.qcmoke.order.controller;
 
+import com.qcmoke.common.entity.User;
 import com.qcmoke.core.utils.RespBean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Slf4j
 @RequestMapping("/order")
 @RestController
 public class OrderController {
+
+    /**
+     * 默认@AuthenticationPrincipal只能注入username，要实现注入用户信息，要配置AccessTokenConverter
+     * 可以使用@AuthenticationPrincipal(expression = "#this.username")获取用户信息中的username
+     */
     @GetMapping(value = "/get/{id}")
-    public RespBean getOrder(@PathVariable("id") Integer id, @AuthenticationPrincipal String username) {
-        return RespBean.ok(username + "查询了订单：order" + id);
+    public RespBean getOrder(@PathVariable("id") Integer id, @AuthenticationPrincipal User user) {
+        log.info("user={}", user);
+        return RespBean.ok(user.getUsername() + "查询了订单：order" + id);
     }
 }
